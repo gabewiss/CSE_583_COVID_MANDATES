00551831225c264f51b679824a9e38fa89327d45
Gabriel <gabew@uw.edu>
2020-11-28 09:51:10 -0800

    Basic uniittest for travis test

--- CSE_583_COVID_MANDATES/tests/test_CSE_583_COVID_MANDATES.py
+++ CSE_583_COVID_MANDATES/tests/test_CSE_583_COVID_MANDATES.py
@@ -1,1 +1,17 @@
-"""This is where the unittest will live"""
+import unittest
+
+import pandas as pd
+
+from CSE_583_COVID_MANDATES import CSE_583_COVID_MANDATES
+
+
+class UnitTests(unittest.TestCase):
+
+    def test_smoke(self):
+        """
+        Testing the travis
+
+        """
+        self.assertTrue(True)
+suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
+_ = unittest.TextTestRunner().run(suite)
