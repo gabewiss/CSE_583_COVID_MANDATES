71e4fe97b7d5ac6ab3fcebab96e26ff69b5b2da2
Gabriel <gabew@uw.edu>
2020-11-29 13:33:40 -0800

    tightened up pep8 syntax to get pylint score out of the red

--- covid/tests/test_covid.py
+++ covid/tests/test_covid.py
@@ -1,15 +1,24 @@
-import unittest
+"""This unittest checks if the function 'covid' function works
+UnitTests(unittest.TestCase) -- framework for functions testing
+                                knn_regression(). Exports 'OK', 'FAIL',
+                                or 'ERROR'
+def test_smoke(self) -- does function run-through w/o problems
+"""
 
 from covid import covid
-
+import unittest
 
 class UnitTests(unittest.TestCase):
+    """
+    Testing the travis
 
+    """
     def test_smoke(self):
         """
         Testing the travis
 
         """
         self.assertTrue(True)
+
 suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
 _ = unittest.TextTestRunner().run(suite)
