6ba63d07eee16f7cc83c2d33c8e79f5d0a80e3e3
Gabriel <gabew@uw.edu>
2020-11-29 13:37:03 -0800

    shortened line 21 so it followed pep8 guidlines. apparently app.py is included in travis

--- covid/app.py
+++ covid/app.py
@@ -18,7 +18,9 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed'
+                 '194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b'
+                 '2826a81f8/gdp-life-exp-2007.csv')
 
 fig = px.scatter(df, x="gdp per capita", y="life expectancy",
                  size="population", color="continent", hover_name="country",
