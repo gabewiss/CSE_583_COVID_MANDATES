d889b612c877059d18488f82f550bed8d59fa851
Gabriel <gabew@uw.edu>
2020-11-28 19:43:28 -0800

    trying the other stuff too

--- CSE_583_COVID_MANDATES/CSE_583_COVID_MANDATES.py
+++
@@ -1,8 +0,0 @@
-def CSE_583_COVID_MANDATES(ya):
-    """
-    Function to get tavisci.yml working. Not working yet. Not working yet.
-    how about now. 4th attempt?
-    """
-
-    ya = ya
-    return ya
--- CSE_583_COVID_MANDATES/__init__.py
+++
@@ -1,4 +0,0 @@
-from .version import __version__
-from .CSE_583_COVID_MANDATES import predict
-
-__all__ = [__version__, predict]
