d7b3f2e18dacba7bb603b45539f70bd2efd2b6a5
Gabriel <gabew@uw.edu>
2020-11-29 13:44:52 -0800

    moved ya = a into test_smoke function from outside. put import ahead of from... import

--- covid/tests/test_covid.py
+++ covid/tests/test_covid.py
@@ -5,19 +5,27 @@ UnitTests(unittest.TestCase) -- framework for functions testing
 def test_smoke(self) -- does function run-through w/o problems
 """
 
-from covid import covid
 import unittest
+from covid import covid
+
 
 class UnitTests(unittest.TestCase):
     """
     Testing the travis
 
     """
+
+
+
     def test_smoke(self):
         """
         Testing the travis
 
         """
+        ya = 3
+
+        covid(ya)
+
         self.assertTrue(True)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
