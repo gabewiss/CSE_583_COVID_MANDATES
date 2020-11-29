3be36fcdc087a201c49e3d36180188f5accd41f0
Gabriel <gabew@uw.edu>
2020-11-28 18:38:01 -0800

    try pushing program COVID_583... to see if removal of libcxx helped

--- CSE_583_COVID_MANDATES/CSE_583_COVID_MANDATES.py
+++ CSE_583_COVID_MANDATES/CSE_583_COVID_MANDATES.py
@@ -1,7 +1,7 @@
 def CSE_583_COVID_MANDATES(ya):
     """
     Function to get tavisci.yml working. Not working yet. Not working yet.
-    how about now?
+    how about now. 4th attempt?
     """
 
     ya = ya
