c2562aa3daf44859a4f15162a8e3bfe5ac470e26
Gabriel <gabew@uw.edu>
2020-12-06 12:31:13 -0800

    pd.read_csv --> read_csv

--- covid/data_retrieval.py
+++ covid/data_retrieval.py
@@ -16,8 +16,8 @@ def data_retrieval():
     """This is a function to pull fresh csv data from two servers"""
 
     """importing newest data from nytimes repo and healthdata.gov"""
-    states = pd.read_csv('https://raw.githubusercontent'  # noqa: F841
+    states = read_csv('https://raw.githubusercontent'  # noqa: F841
                          '.com/nytimes/covid-19-data/master/us-states.csv')
-    mandates = pd.read_csv('https://healthdata.gov'  # noqa: F841
+    mandates = read_csv('https://healthdata.gov'  # noqa: F841
                            '/sites/default/files/'
                            'state_policy_updates_20201202_0721.csv')
