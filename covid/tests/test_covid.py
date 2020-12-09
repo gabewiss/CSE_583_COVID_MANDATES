96ae919118ae9a8c1ebe9bad8bc09fd4a3667b59
Gabriel <gabew@uw.edu>
2020-12-09 14:40:57 -0800

    changed to self.assertTrue

--- covid/tests/test_covid.py
+++ covid/tests/test_covid.py
@@ -25,7 +25,7 @@ class CovidTest(unittest.TestCase):
 
         not_important = 3
 
-        self.assertEqual(covid.weee(not_important), 3)
+        self.assertTrue(covid.weee(not_important),)
 
 
 if __name__ == '__main__':
